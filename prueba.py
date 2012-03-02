7f55c6a9fa875e049bbcce51eee3498defbd80f2
Martin Lavecchia <lavecchia@gmail.com>
2012-03-02 19:55:14 +0100

    me voy

--- prueba.py
+++ prueba.py
@@ -1,7 +1,38 @@
 import read_mol2
+import search_rotatable
+import matrix_rotation
 
 data = open("pentane.mol2", 'r')
 
 prueba = read_mol2.ReadMol2(data)
 
-print prueba.get_bonded_atoms(3)
+#print str(prueba.atoms[1].atom_type)
+
+#for i in range(0,prueba.num_atoms):
+#	print prueba.atoms[i].atom_type
+	
+#print prueba.atoms[2]
+#print prueba.get_atom(2).atom_type
+
+
+#print prueba.get_bonded_atoms(14)[0].atom_id
+#print prueba.get_bonded_atoms(14)[1].atom_id
+#print prueba.get_bonded_atoms(14)[2].atom_id
+#print prueba.get_bonded_atoms(14)[3].atom_id
+
+
+
+#print prueba.get_bonded_atoms(17)[0].atom_id
+#print "longitud:" + str(len(prueba.get_bonded_atoms(14)))
+#print prueba.bonds[1]
+
+print search_rotatable.SearchRotatable(prueba)
+
+bonds = []
+
+for ind in range(0,prueba.num_bonds):
+    bonds.append([prueba.bonds[ind].origin_atom_id,prueba.bonds[ind].target_atom_id])
+
+cmatrix = matrix_rotation.ConnectivityMatrix(prueba.num_atoms,prueba.num_bonds, bonds)
+
+print cmatrix.connectivity_matrix()
